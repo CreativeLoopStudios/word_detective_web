1c0f8a3f4de212f37d5962d35f9a10db9eb0e5d6
Giulio Bottari <giuliobottari@gmail.com>
2021-03-11 20:30:39 -0300

    Add MainContainer component

---
+++ project/src/components/MainContainer.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Grid, makeStyles, Paper } from "@material-ui/core";
+
+const useTypes = makeStyles(() => ({
+    paper: {
+        maxWidth: 900,
+        minWidth: 500,
+        minHeight: 300,
+        borderRadius: '1rem',
+        backgroundColor: 'rgba(0,0,0,.8)',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+}));
+
+type Props = {
+    children?: React.ReactNode,
+}
+
+export const MainContainer = ({ children }: Props) => {
+    const classes = useTypes();
+    return (
+        <Paper elevation={1} className={classes.paper}>
+            { children }
+        </Paper>
+    );
+}
+
+MainContainer.propTypes = {}
+MainContainer.defaultProps = {}
