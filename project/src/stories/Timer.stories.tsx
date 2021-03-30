6f368056b73b370d4c658a811a56999a7548a9e0
Giulio Bottari <giuliobottari@gmail.com>
2021-03-30 17:03:52 -0300

    Fix timer background on storybook

--- project/src/stories/Timer.stories.tsx
+++ project/src/stories/Timer.stories.tsx
@@ -7,19 +7,24 @@ import { Props } from '../components/Timer'
 export default {
   title: 'WD/Timer',
   component: Timer,
+  parameters: {
+    default: "dark",
+    values: [{ name: "dark", value: "#333333" }],
+  },
   argTypes: {
     color: { control: 'color' }
   }
 };
 
 const Template: Story<Props> = (args) => (
-  <div style={{backgroundColor: 'gray'}}>
     <Timer {...args} />
-  </div>
 )
 
 export const Default = Template.bind({});
 Default.args = {
   value: 14,
   max: 15
+}
+Default.parameters = {
+    backgrounds: { default: 'dark' }
 }
