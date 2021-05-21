078252c562b499a8bea7cb5726bf1cfd7fdcbfc3
Giulio Bottari <giuliobottari@gmail.com>
2021-05-20 23:10:43 -0300

    Optimize timer by moving in the countdown

--- project/src/components/Timer.tsx
+++ project/src/components/Timer.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Box, CircularProgress, makeStyles } from "@material-ui/core";
+import { useCountdown } from '../hooks';
 
 export type Props = {
-    value: number;
     max: number;
+    key: any;
     color?: string;
+    onExpire?: () => void;
 }
 
 type StyleProps = {
@@ -24,17 +26,20 @@ const useStyles = makeStyles(() => ({
     })
 }));
 
-function Timer({ value, max, color }: Props) {
+function Timer({ max, color, onExpire, key }: Props) {
     const classes = useStyles({ color: color || '#FF0D0D' });
+    const { countdown, start, stop } = useCountdown();
+    const [key_, setKey] = useState(0);
 
-    if (value > max) {
-        value = max;
-    } else if (value < 0) {
-        value = 0;
+    if (key !== key_ && key !== 0) {
+        stop();
+    }
+    if (key !== key_ && max > 0) {
+        start(max, onExpire || (() => {}));
+        setKey(key);
     }
 
-    const timerVal = value * 100 / max;
-
+    const timerVal = countdown * 100 / max;
     return (
     <Box position="relative" display="inline-flex">
         <CircularProgress className={classes.progress} variant="determinate" value={timerVal} size={90} thickness={7} />
@@ -48,18 +53,13 @@ function Timer({ value, max, color }: Props) {
         alignItems="center"
         justifyContent="center"
         >
-            <span className={classes.root}>{value}</span>
+            <span className={classes.root}>{countdown}</span>
         </Box>
     </Box>
     );
 }
 
 Timer.propTypes = {
-    /**
-     * The current value of the timer.
-     */
-    value: PropTypes.number.isRequired,
-
     /**
      * The max value of the timer.
      */
@@ -69,6 +69,17 @@ Timer.propTypes = {
      * Progress color.
      */
     color: PropTypes.string,
+
+
+    /**
+     * Called when expired.
+     */
+    onExpire: PropTypes.func,
+
+    /**
+     * Change this to force reseting the timer.
+     */
+    key: PropTypes.any,
 };
 
 export default Timer;
