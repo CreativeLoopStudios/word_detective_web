e561ea34cc07c83fe8d5e527d7dcc853bc6333b6
Lucas Reis <lucasreis3000@gmail.com>
2021-06-21 15:29:30 -0300

    inserting online status of player on PlayerRankingItem

--- project/src/components/PlayerRankingItem.tsx
+++ project/src/components/PlayerRankingItem.tsx
@@ -1,25 +1,53 @@
 import React from 'react';
 
-import { Grid } from "@material-ui/core";
+import PropTypes from 'prop-types';
+
+import { makeStyles, Grid } from "@material-ui/core";
 
 import PlayerIcon from './PlayerIcon'
 import Label from './Label';
 
+const useStyles = makeStyles(() => ({
+    icon: {
+        position: 'relative'
+    },
+    circleStatus: {
+        width: '1rem',
+        height: '1rem',
+        borderRadius: '10rem',
+        position: 'absolute',
+        bottom: '0rem',
+        right: '0.5rem',
+        border: '2px solid white'
+    },
+    circleOnline: {
+        backgroundColor: 'green'
+    },
+    circleOffline: {
+        backgroundColor: 'red'
+    }
+}));
+
 export type Props = {
     isWordMaster: boolean;
+    isOnline: boolean;
     playerName: string;
     score: number;
     className: string;
 }
 
-function PlayerRankingItem({ isWordMaster, playerName, score, className }: Props) {
+function PlayerRankingItem({ isWordMaster, isOnline, playerName, score, className }: Props) {
+    const classes = useStyles();
+
     return (
         <Grid container item alignItems="center" xs={12} className={className}>
-            <Grid item xs={4}>
+            <Grid item xs={5} className={classes.icon}>
                 <PlayerIcon isWordMaster={isWordMaster} />
+
+                <div className={`${classes.circleStatus} ${isOnline ? classes.circleOnline : classes.circleOffline}`}></div>
             </Grid>
 
-            <Grid container item direction="column" xs={8}>
+            <Grid container item direction="column" xs={7}>
                 <Grid item>
                     <Label kind="secondary">{playerName}</Label>
                 </Grid>
@@ -33,7 +61,20 @@ function PlayerRankingItem({ isWordMaster, playerName, score, className }: Props
     );
 }
 
-PlayerRankingItem.propTypes = {};
-PlayerRankingItem.defaultProps = {};
+PlayerRankingItem.propTypes = {
+    isWordMaster: PropTypes.bool,
+    isOnline: PropTypes.bool,
+    playerName: PropTypes.string,
+    score: PropTypes.number,
+    className: PropTypes.string
+};
+
+PlayerRankingItem.defaultProps = {
+    isWordMaster: false,
+    isOnline: false,
+    playerName: undefined,
+    score: 0,
+    className: undefined
+};
 
 export default PlayerRankingItem;
